08548886369a5499efc8011b417a61cda572874e
twinkle-thakur <twinklethakur7879@gmail.com>
2025-04-14 19:58:28 +0530

    Callable Example

--- src/multiThreading/CallableThreadTest.java
+++ src/multiThreading/CallableThreadTest.java
@@ -1,9 +1,17 @@
 package multiThreading;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class CallableThreadTest {
-public static void main(String[] args) {
+public static void main(String[] args) throws InterruptedException, ExecutionException {
 	CallableThread adderThread=new CallableThread();
-	
+	ExecutorService service = Executors.newFixedThreadPool(1);
+	Future<Integer> future = service.submit(adderThread);
+	System.out.println(future.get());
+	service.shutdown();
 	
 }
 }
--- src/multiThreading/CallableThreadUsingLambda.java
+++ src/multiThreading/CallableThreadUsingLambda.java
@@ -1,6 +1,9 @@
 package multiThreading;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class CallableThreadUsingLambda {
 public static void main(String[] args) throws Exception {
@@ -13,6 +16,9 @@ public static void main(String[] args) throws Exception {
 		}
 		return sum;
 	};
-	
+	ExecutorService service = Executors.newFixedThreadPool(1);
+	Future<Integer> future = service.submit(callableThread);
+	System.out.println(future.get());
+	service.shutdown();
 }
 }
